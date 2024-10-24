41ffdca0b62dcd73a31c57bd2a9516623bc1cd94
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-10-24 15:53:03 +0900

    [fix] : 버튼 컴포넌트 타입 수정

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,9 @@
-export default function Button(text: string, bg: string) {
+type Props = {
+  text: string;
+  bg: string;
+};
+
+export default function Button({ text, bg }: Props) {
   return (
     <button className={`border border-black ${bg} p-2 rounded-lg`}>
       {text}
