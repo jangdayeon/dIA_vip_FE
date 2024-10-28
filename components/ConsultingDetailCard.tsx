b65fa68fa8cf4e883ee4eb14a3c21313b661f5a4
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2024-10-28 16:04:14 +0900

    [feat] : Sidebar & 상담 id별로 detail 페이지 추가

--- components/ConsultingDetailCard.tsx
+++ components/ConsultingDetailCard.tsx
@@ -1,24 +1,42 @@
 'use client';
 
-import {
-  ConsultingDetail,
-  consultingDetailData,
-} from '@/data/consultingdetaildata';
 import { FolderArrowDownIcon, HeartIcon } from '@heroicons/react/16/solid';
-import { useRouter } from 'next/navigation';
+import { useRouter, useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+interface ConsultingDetail {
+  id: number;
+  category: string;
+  title: string;
+  date: string;
+  manager: string;
+  status: string;
+  content: string;
+}
 
 export default function ConsultingDetailCard() {
-  //TODO: id를 통해 consultingDetail 찾기 구현
-  const {
-    category,
-    title,
-    pb_name,
-    customer_name,
-    date,
-    content,
-  }: ConsultingDetail = consultingDetailData;
-
+  const { id } = useParams();
+  const [consultingDetail, setConsultingDetail] =
+    useState<ConsultingDetail | null>(null);
   const router = useRouter();
+
+  useEffect(() => {
+    async function fetchConsultingDetail() {
+      const response = await fetch('/api/searchResults');
+      const data = await response.json();
+      const detail = data.searchResults.find(
+        (item: ConsultingDetail) => item.id === Number(id)
+      );
+
+      if (detail) setConsultingDetail(detail);
+    }
+
+    fetchConsultingDetail();
+  }, [id]);
+
+  if (!consultingDetail) return <div>Loading...</div>;
+
+  const { category, title, date, manager } = consultingDetail;
 
   return (
     <div className='mt-10 mx-20'>
@@ -44,10 +62,10 @@ export default function ConsultingDetailCard() {
             <div className='text-2xl font-bold'>
               [{category}] {title}
             </div>
-            <div className='flex justify-between py-2'>
+            <div className='flex justify-between py-2 space-x-10'>
               <div className='flex flex-row gap-2'>
-                <div className='font-bold'>PB명</div> {pb_name}
-                <div className='font-bold'>고객명</div> {customer_name}
+                <div className='font-bold'>PB명</div> {manager}
+                <div className='font-bold'>고객명</div>
               </div>
               <div>{date}</div>
             </div>
@@ -55,11 +73,11 @@ export default function ConsultingDetailCard() {
 
           <div className='bg-white w-full p-5 border-b'>
             <div className='text-xl font-bold'>상담 내용</div>
-            <div className='mt-5'>{content}</div>
+            <div className='mt-5'>상세 상담 내용이 여기 표시됩니다.</div>
           </div>
           <div className='bg-white w-full p-5 border-b'>
             <div className='text-xl font-bold'>상담 주요 스크립트</div>
-            <div className=''>{content}</div>
+            <div className='mt-5'>상담 스크립트가 여기 표시됩니다.</div>
           </div>
         </div>
       </div>
