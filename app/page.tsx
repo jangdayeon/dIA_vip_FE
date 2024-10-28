64723cbd5c673583858fd922e9de60d8eb39fe32
jangdayeon <dayeonjang.dev@gmail.com>
2024-10-28 16:21:13 +0900

    [feat] : 로그인 전 메인페이지 퍼블 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,10 @@
 'use client';
 
-import ConsultingListCard from '@/components/ConsultingListCard';
-import PBCard from '@/components/PBCard';
-import ReserveCard from '@/components/ReserveCard';
-import ScheduleCard from '@/components/ScheduleCard';
-import Image from 'next/image';
-import banner from '../assets/home_banner.png';
+import LoginedMain from '@/components/LoginedMain';
+import NotLoginedMain from '@/components/NotLoginedMain';
 
 export default function Home() {
-  return (
-    <div className='flex justify-center items-center mx-auto p-10'>
-      <div className='bg-sky-50 rounded-2xl'>
-        <Image src={banner} alt='' className='w-full' />
-
-        <div className='grid grid-cols-3 gap-3 p-5'>
-          <div className='grid grid-cols-1 gap-3'>
-            <PBCard />
-            <ReserveCard />
-          </div>
-
-          <ScheduleCard />
-          <ConsultingListCard />
-        </div>
-      </div>
-    </div>
-  );
+  //TODO: 로그인 연결 후 처리 필요
+  const logined = true;
+  return <>{logined ? <LoginedMain /> : <NotLoginedMain />}</>;
 }
---
+++ components/NotLoginedMain.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import hanaCharacters from '../assets/hanaCharacters.png';
+import notLoginedBackgroundImg from '../assets/notLoginedBackgroundImg.png';
+import notLoginedText from '../assets/notLoginedText.png';
+
+export default function Logined() {
+  return (
+    <div className='relative h-screen w-full'>
+      <Image
+        src={notLoginedBackgroundImg}
+        alt='Background'
+        layout='fill'
+        objectFit='cover'
+        quality={100}
+        className='-z-10' // 뒤에 배치
+      />
+      <div className='relative z-10'>
+        <div className='flex flex-row items-center w-full p-32'>
+          <Image
+            className='w-1/2'
+            src={notLoginedText}
+            alt={
+              '하나금융그룹의 VIP 손님을 위한 180°달라질 PB (Private Banker) 상담 서비스'
+            }
+          />
+          <Image className='w-1/2' src={hanaCharacters} alt={'하나캐릭터들'} />
+        </div>
+      </div>
+    </div>
+  );
+}
