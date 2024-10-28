1baa0660f27c3b1569068560bad8767d6a36c156
jangdayeon <dayeonjang.dev@gmail.com>
2024-10-28 12:56:57 +0900

    [feat] : consultingdetail component 추가

--- app/(routes)/consultingList/[id]/page.tsx
+++ app/(routes)/consultingList/[id]/page.tsx
@@ -1,7 +1,10 @@
+import ConsultingDetailCard from '@/components/ConsultingDetailCard';
+
 export default function Detail() {
   return (
     <div>
       <div>상담 내역 ID 상세 내역 </div>
+      <ConsultingDetailCard />
     </div>
   );
 }
