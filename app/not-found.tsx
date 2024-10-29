2b3b34f7b30454c0326f7bfdc3615fd861cd1c7f
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2024-10-29 15:11:41 +0900

    [feat] : 404 Not Found 페이지 디자인

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,20 @@
 'use client';
 
+import Image from 'next/image';
+import Link from 'next/link';
+import notFound from '../assets/notFound.gif';
+
 export default function NotFound() {
-  return <>404 Not Found!</>;
+  return (
+    <div className='flex flex-col items-center justify-center h-screen text-center p-6'>
+      <Image src={notFound} alt='Not Found GIF' width={200} height={200} />
+      <h1 className='text-6xl font-bold text-gray-800 mb-4'>404</h1>
+      <p className='text-xl text-gray-600 mb-8'>페이지를 찾을 수 없습니다.</p>
+      <Link href='/' legacyBehavior={true}>
+        <a className='px-6 py-3 border border-blue-200 text-blue-500 font-semibold rounded-xl hover:bg-blue-200 transition duration-200'>
+          홈으로
+        </a>
+      </Link>
+    </div>
+  );
 }
