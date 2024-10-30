8a10b4777d26ee3083569c17d0bef820f4012430
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-10-30 10:20:54 +0900

    [feat] : 상담 예약 캘린더 기능 추가

--- app/(routes)/consultingList/page.tsx
+++ app/(routes)/consultingList/page.tsx
@@ -26,41 +26,46 @@ export default function ConsultingList() {
       <div className='my-1 text-gray-600'>
         하나은행만의 전문 PB와 상담한 내역을 확인하실 수 있습니다.
       </div>
-      <form className='mt-10 p-5 border border-black bg-[#D6E8F6] rounded-lg'>
+      <form className='mt-10 p-5 border border-black bg-[#D6E8F6] rounded-lg w-full'>
         <div className='flex w-full items-center gap-2 my-2'>
           <p className='w-32 text-center font-semibold'>검색</p>
-          <input
-            type='text'
-            className='border border-black bg-white rounded-lg w-full p-1'
-            placeholder='검색 키워드를 입력해주세요.'
-          />
-          <button className='border border-black p-1 rounded-lg bg-gray-300 hover:bg-gray-400 hover:text-white'>
-            <Search />
-          </button>
+          <div className='flex w-full justify-between mr-1 items-center'>
+            <input
+              type='text'
+              className='border border-black bg-white rounded-lg w-full p-1'
+              placeholder='검색 키워드를 입력해주세요.'
+            />
+            <button className='border border-black p-1 rounded-lg bg-gray-300 hover:bg-gray-400 hover:text-white ml-2'>
+              <Search />
+            </button>
+          </div>
         </div>
         <div className='flex w-full items-center gap-2 my-2'>
-          <p className='w-40 text-center font-semibold'>카테고리</p>
-          <select
-            ref={categoryRef}
-            defaultValue='전체'
-            className='border bg-white rounded-lg p-1 border-black w-80'
-          >
-            <option value='전체'>전체</option>
-            {categories.map((category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
-          <p className='w-28 text-center font-semibold'>날짜 설정</p>
-          <div className='flex border bg-white rounded-lg p-1 gap-3 border-black'>
-            <input ref={dateStartRef} type='date' className='w-40' />
-            <p>~</p>
-            <input ref={dateEndRef} type='date' className='w-40' />
+          <p className='w-32 text-center font-semibold'>카테고리</p>
+          <div className='flex w-full mr-1 items-center '>
+            <select
+              ref={categoryRef}
+              defaultValue='전체'
+              className='border bg-white rounded-lg p-1.5 border-black w-40'
+            >
+              <option value='전체'>전체</option>
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+            <div className='flex w-full items-center gap-2 justify-end'>
+              <p className='text-center font-semibold mx-2 w-auto'>날짜 설정</p>
+              <div className='flex flex-wrap border bg-white rounded-lg p-1 gap-3 border-black'>
+                <input ref={dateStartRef} type='date' className='' />
+                <input ref={dateEndRef} type='date' className='' />
+              </div>
+              <button className='border border-black p-1 rounded-lg bg-gray-300 hover:bg-gray-400 hover:text-white'>
+                <RotateCcw />
+              </button>
+            </div>
           </div>
-          <button className='border border-black p-1 rounded-lg bg-gray-300 hover:bg-gray-400 hover:text-white'>
-            <RotateCcw />
-          </button>
         </div>
         <SearchResult />
       </form>
--- utils/date.ts
+++ utils/date.ts
@@ -1,28 +1,24 @@
 /**
  * Date 객체를 날짜 형식의 string으로 지정해주는 함수입니다.
- * 형식은 yyyyMMdd HH:mm 입니다.
+ * 형식은 yyyyMMdd 입니다.
  * JSON으로 날짜를 보낼 일이 생긴다면 밑의 formatDate 함수를 사용해주시길 바랍니다.
  */
 export function formatDate(date: Date): string {
   const year = date.getFullYear().toString();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
 
-  return `${year}${month}${day} ${hours}:${minutes}`;
+  return `${year}${month}${day}`;
 }
 
 /**
- * yyyyMMdd HH:mm 형식으로 생성된 날짜 string을 Date 객체로 바꾸는 함수입니다.
+ * yyyyMMdd 형식으로 생성된 날짜 string을 Date 객체로 바꾸는 함수입니다.
  * JSON으로 받은 날짜를 Date 객체로 관리하고 싶다면 밑의 parseFormattedDate 함수를 사용해주시길 바랍니다.
  */
 export function parseFormattedDate(dateString: string): Date {
   const year = parseInt(dateString.slice(0, 4), 10);
   const month = parseInt(dateString.slice(4, 6), 10) - 1;
   const day = parseInt(dateString.slice(6, 8), 10);
-  const hours = parseInt(dateString.slice(9, 11), 10);
-  const minutes = parseInt(dateString.slice(12, 14), 10);
 
-  return new Date(year, month, day, hours, minutes);
+  return new Date(year, month, day);
 }
