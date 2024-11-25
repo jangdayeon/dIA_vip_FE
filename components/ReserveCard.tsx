f9a6fb66a1086bc973526a23e1f5628170ccd075
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2024-11-25 17:14:03 +0900

    [feat] : 메인페이지 상담버튼 디자인 수정

--- components/ReserveCard.tsx
+++ components/ReserveCard.tsx
@@ -1,4 +1,5 @@
-import { ChevronRightIcon } from '@heroicons/react/24/outline';
+// import { ChevronRightIcon } from '@heroicons/react/24/outline';
+import { CalendarPlus, PhoneCall, ChevronRight } from 'lucide-react';
 import Link from 'next/link';
 
 export default function ReserveCard() {
@@ -7,18 +8,36 @@ export default function ReserveCard() {
       <Link href='/reserve'>
         <div className='flex justify-between border-b border-opacity-55 px-6 py-4 hover:bg-gray-100'>
           <h2 className='text-slate-600 text-2xl font-semibold'>상담 예약</h2>
-          <ChevronRightIcon className='h-8 text-[#3F6886]' />
         </div>
       </Link>
 
-      <div className='flex px-6'>
-        <div className='mt-4 space-y-2'>
-          <div className='text-stone-900 text-xl font-normal'>
-            맞춤 예약 서비스
+      <div className='p-6 space-y-2'>
+        <div className='flex justify-between items-center border border-blue-100 text-gray-700 hover:bg-blue-200 p-2 rounded-lg font-semibold'>
+          <div className='flex items-center space-x-2'>
+            <PhoneCall />
+            <div>
+              <div>빠른 상담</div>
+              <div className='text-black/60 text-xs font-normal'>
+                담당 PB가 상담 가능할 경우, 바로 상담을 신청하여 간단한 용무를
+                처리할 수 있습니다.
+              </div>
+            </div>
           </div>
-          <div className='text-black/60 text-xs font-normal'>
-            하나은행만의 전문 PB와 1대 1 상담을 언제든 신청할 수 있습니다.
+          <ChevronRight className='text-[#3F6886]' />
+        </div>
+
+        <div className='flex justify-between items-center border border-blue-100 text-gray-700 hover:bg-blue-200 p-2 rounded-lg font-semibold'>
+          <div className='flex items-center space-x-2'>
+            <CalendarPlus />
+            <div>
+              <div>자세한 상담</div>
+              <div className='text-black/60 text-xs font-normal'>
+                하나은행만의 전문 PB와 1대 1 심층 상담을 언제든 예약할 수
+                있습니다.
+              </div>
+            </div>
           </div>
+          <ChevronRight className='text-[#3F6886]' />
         </div>
       </div>
     </div>
